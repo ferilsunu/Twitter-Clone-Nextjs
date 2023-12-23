e2ef98df36be911406c191d2e453d0b9130908da
Feril Sunu <ferilsunu@gmail.com>
2023-12-23 19:08:19 +0530

    vercel update

--- vercel-build.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-
-try {
-  // Run Prisma generate command
-  execSync('npx prisma generate', { stdio: 'inherit' });
-} catch (error) {
-  console.error('Error running Prisma generate command:', error);
-  process.exit(1);
-}
