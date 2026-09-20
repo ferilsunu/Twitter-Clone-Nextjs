b63049c7b9a46ee7a5108ccd3ad9306f6fb07845
Feril Sunu <feril@ferilsunu.com>
2026-09-20 12:40:13 +0400

    feat: add high-resolution Twitter favicons and touch icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,8 +6,12 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet" />
-        <meta name="theme-color" content="#0284c7" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#1d9bf0" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
